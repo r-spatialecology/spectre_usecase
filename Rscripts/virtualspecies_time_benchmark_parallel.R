583c8eb720ad6576c9aecc5167d87f19ad8dccd3
nldoc <jasa@posteo.de>
2020-09-18 09:21:33 +0200

    benchmark plot

--- Rscripts/virtualspecies_time_benchmark_parallel.R
+++ Rscripts/virtualspecies_time_benchmark_parallel.R
@@ -65,8 +65,8 @@ if (exec == "HPC") {
                                           log_file = "spectre.log", # define logfile name
                                           queue = "medium",  # define HPC queue
                                           service = "normal", # define HPC service
-                                          walltime = "48:00:00", # define walltime
-                                          n_cpu = 12,
+                                          walltime = "04:00:00", # define walltime
+                                          n_cpu = "12",
                                           mem_cpu = "4000")) # define memory per cpu   
   results <- dplyr::bind_rows(results)
 }
@@ -78,13 +78,13 @@ if (exec == "HPC") {
 #})
 
 #### STORE RESULTS:
-saveRDS(results, file=file.path(dir.cloud, "data", "virtualspecies_benchmark.rds"))
+saveRDS(results, file=file.path(dir.cloud, "data", "virtualspecies_benchmark_parallel.rds"))
 
 
 #### Examine results:
 #
 #
-results <- readRDS("data/virtualspecies_benchmark.rds")
+results <- readRDS("data/virtualspecies_benchmark_parallel.rds")
 
 ## Projected time for 50000 iterations:
 
@@ -140,3 +140,57 @@ results %>%
   ggthemes::theme_tufte(base_size = 12)
 
 ggsave(filename="benchmark_hpc_12cores.png", path = "figures", width=6, height=6, dpi=300)
+
+
+##############################
+### COMPARE BOTH APPROACHES:
+#### Examine results:
+#
+#
+results_singlecore <- readRDS("data/virtualspecies_benchmark.rds")
+results_parallel <- readRDS("data/virtualspecies_benchmark_parallel.rds")
+
+results_singlecore$job <- "singlecore"
+results_parallel$job <- "parallel"
+
+results <- rbind(results_singlecore, results_parallel) %>% 
+  dplyr::filter(landscape_size %in% c(10, 50, 75))
+
+
+## Projected time for 50000 iterations:
+
+results %>% 
+  dplyr::select(job, gamma, landscape_size, bench_total) %>% 
+  dplyr::mutate(bench_total = as.numeric((bench_total / 60) / 60)) %>% 
+  ggplot(., aes(x=gamma, y=bench_total, color=factor(landscape_size), group=landscape_size)) +
+  facet_wrap(~job) +
+  geom_line(size=1) +
+  geom_point(size=2) +
+  #geom_bar(stat="identity", position="dodge") +
+  xlab("Gamma") +
+  ylab("Time per 100 iterations [hrs]") +
+  #guides(color=guide_legend(title="Landscape size")) +
+  ggsci::scale_color_jco() +
+  ggthemes::theme_tufte(base_size = 12)
+
+## Projected for 50k:
+results %>% 
+  dplyr::select(job, gamma, landscape_size, bench_total) %>% 
+  dplyr::mutate(bench_total = as.numeric((((bench_total / max_runs) * 50000 )/ 60) / 60)) %>% 
+  ggplot(., aes(x=gamma, y=bench_total)) +
+  facet_wrap(~job) +
+  geom_line(size=1, aes(color=factor(landscape_size), group=landscape_size)) +
+  geom_point(size=2, aes(color=factor(landscape_size), group=landscape_size)) +
+  geom_hline(yintercept = 48, lty=2) +
+  geom_label(x=350, y=48, label="48hrs") +
+  geom_hline(yintercept = 120, lty=2) +
+  geom_label(x=350, y=120, label="120hrs") +
+  xlab("Gamma") +
+  ylab("Time per 50k iterations [hrs]") +
+  guides(color=guide_legend(title="Landscape size")) +
+  ggsci::scale_color_jco() +
+  ggthemes::theme_tufte(base_size = 12)
+
+ggsave(filename="benchmark_hpc_parallel.png", path = "figures", width=6, height=6, dpi=300)
+
+
