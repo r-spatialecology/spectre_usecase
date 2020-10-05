74f7c4dacfb02dc24c232e3c3d857447aee3211d
nldoc <jasa@posteo.de>
2020-10-05 16:14:07 +0200

    :bug: fixed rce and mae calculation in virtualspecies vignettes

---
+++ R/calculate_mae.R
@@ -0,0 +1,31 @@
+#' @title calculate_mae
+#' 
+#' @description The function calculates the real commonness error
+#' 
+#' @param target The target (commonness matrix) that was provided for the spectre optimization algorithm
+#' @param optimized_grid The final grid (species X site) that was reported by the spectre algoithm
+#' 
+#' @details 
+#' 
+#' @return mae real commonness error
+#' @examples 
+#' \dontrun{
+#' 
+#' 
+#' 
+#' }
+#' @export
+
+calculate_mae <- function(target, optimized_grid)
+{
+  solution_commonness <- spectre:::calculate_solution_commonness_rcpp(optimized_grid)
+  target_commonness <- target 
+  
+  # Calculate mean:
+  target_mean_commonness <- mean(abs(target), na.rm = TRUE)
+  
+  # Calculate mean difference:
+  mae <- mean(abs(solution_commonness - target_commonness), na.rm = TRUE)
+  
+  return(mae)
+}
---
+++ R/calculate_rce.R
@@ -0,0 +1,36 @@
+#' @title calculate_rce
+#' 
+#' @description The function calculates the relative commonness error
+#' 
+#' @param target The target (commonness matrix) that was provided for the spectre optimization algorithm
+#' @param optimized_grid The final grid (species X site) that was reported by the spectre algoithm
+#' 
+#' @details 
+#' 
+#' 
+#' @return rce relative commonness error
+#' @examples 
+#' \dontrun{
+#' 
+#' 
+#' 
+#' }
+#' @export
+
+calculate_rce <- function(target, optimized_grid)
+{
+  
+  solution_commonness <- spectre:::calculate_solution_commonness_rcpp(optimized_grid)
+  target_commonness <- target 
+  
+  # Calculate mean:
+  target_mean_commonness <- mean(abs(target), na.rm = TRUE)
+  
+  # Calculate mean difference:
+  rce <- mean(abs(solution_commonness - target_commonness), na.rm = TRUE)
+  
+  # Weight rleative to target mean:
+  rce <- rce / target_mean_commonness 
+  
+  return(rce)
+}
